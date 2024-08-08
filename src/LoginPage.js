16a2860168c0a4fffd58cfb5658ffda656f17190
minseoky <minseok184@gmail.com>
2024-08-08 10:10:08 +0900

    feat: kak ao oauth 로직 추가

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -5,20 +5,15 @@ function LoginPage() {
         window.location.href = "http://localhost:8080/oauth2/authorization/google";
     };
 
-    React.useEffect(() => {
-        // URL에서 JWT를 추출하여 localStorage에 저장
-        const urlParams = new URLSearchParams(window.location.search);
-        const jwt = urlParams.get('jwt');
-        if (jwt) {
-            localStorage.setItem('jwt', jwt);
-            window.location.href = '/home';  // JWT 저장 후 홈 페이지로 리다이렉트
-        }
-    }, []);
+    const kakaoLogin = () => {
+        window.location.href = "http://localhost:8080/oauth2/authorization/kakao";
+    };
 
     return (
         <div>
             <h1>Login</h1>
             <button onClick={googleLogin}>Login with Google</button>
+            <button onClick={kakaoLogin}>Login with Kakao</button>
         </div>
     );
 }
